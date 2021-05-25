0f1bc0af974f6fc2aadcb1261eb8647e1362a4cd
SergheiCucerovschii <74251573+SergheiCucerovschii@users.noreply.github.com>
2021-05-25 12:33:09 +0300

    Add files via upload

---
+++ DZexceptions.py
@@ -0,0 +1,20 @@
+def intInput():
+    index = int(input("Enter an index: "))
+    return index
+
+
+def getData( index ):
+  data = [10,20,30,40,50]
+  return data[index]
+
+def printRes():
+   print(getData(index))
+
+try:
+    index = intInput()
+    data = getData( index )
+    printRes()
+except ValueError:
+    print("index cannot be something else than an integer")
+except IndexError:
+    print("an index value cannot be outside the list")
